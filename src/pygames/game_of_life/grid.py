918cf3579b3dc1cb8690ce30482b4dc876e1f9b7
Kai Jie Chow <kj1chow@gmail.com>
2026-02-06 16:32:12 +0000

    grid refactoring

--- src/pygames/game_of_life/grid.py
+++ src/pygames/game_of_life/grid.py
@@ -1,28 +1,54 @@
-from typing import List
+import dataclasses
+from typing import Any, List, Tuple
 from tabulate import tabulate
 
 
-class Grid:
-    def __init__(self, height: int, width: int) -> None:
-        self.__height = height
-        self.__width = width
-        self.__grid = {(i, j): False for i in range(height) for j in range(width)}
-
-    @property
-    def grid(self) -> List[List[bool]]:
-        return [
-            [self.__grid[(i, j)] for j in range(self.__width)]
-            for i in range(self.__height)
-        ]
-
-    def toggle(self, row: int, col: int) -> None:
-        if row >= self.__height or col >= self.__width:
-            raise IndexError
-        coord = (row, col)
-        self.__grid[coord] = not self.__grid[coord]
+@dataclasses.dataclass
+class Cell:
+    coord: Tuple[int, int]
+    value: bool
 
     def __repr__(self) -> str:
-        data = [[int(j) for j in i] for i in self.grid]
-        col_indices = ["", *list(range(self.__width))]
+        return str(self.value)
+
+
+class Grid:
+    def __init__(self, *rows: List["Cell"]) -> None:
+        self.__validate(*rows)
+        self.__height = len(rows)
+        self.__width = len(rows[0]) if rows else 0
+        self.__grid = rows
+
+    @property
+    def height(self) -> int:
+        return self.__height
+
+    @property
+    def width(self) -> int:
+        return self.__width
+
+    @property
+    def grid(self) -> List["Cell"]:
+        return [cell for row in self.__grid for cell in row]
+
+    def __eq__(self, other: "Any") -> bool:
+        if isinstance(other, Grid):
+            return self.grid == other.grid
+        return False
+
+    def __repr__(self) -> str:
+        data = self.__grid
+        col_indices = ["", *list(range(self.width))]
         table = tabulate(data, headers=col_indices, showindex="always", tablefmt="grid")
         return str(table)
+
+    def __validate(self, *rows: List["Cell"]) -> None:
+        height, width = len(rows), len(rows[0]) if rows else 0
+        coords = [cell.coord for row in rows for cell in row]
+        if coords != [(i, j) for i in range(height) for j in range(width)]:
+            raise ValueError("invalid cell arrangement")
+
+
+def build_empty_grid(height: int, width: int) -> "Grid":
+    grid = [[Cell((i, j), False) for j in range(width)] for i in range(height)]
+    return Grid(*grid)
--- tests/test_grid.py
+++ tests/test_grid.py
@@ -1,22 +1,60 @@
 import pytest
-from pygames.game_of_life import grid as grid_
+from pygames.game_of_life.grid import *
 
 
 class TestGrid:
     @pytest.fixture
-    def grid(self) -> grid_.Grid:
-        return grid_.Grid(3, 2)
+    def grid(self) -> Grid:
+        return build_empty_grid(3, 2)
 
-    def test_grid(self, grid: grid_.Grid) -> None:
-        expected = [[False, False], [False, False], [False, False]]
-        assert grid.grid == expected
+    def test_grid_initialisation(self) -> None:
+        Grid()
+        Grid(
+            [Cell((0, 0), False), Cell((0, 1), True)],
+            [Cell((1, 0), True), Cell((1, 1), False)],
+        )
 
-    def test_toggle(self, grid: grid_.Grid) -> None:
-        grid.toggle(1, 1)
-        grid.toggle(0, 0)
-        assert grid.grid == [[True, False], [False, True], [False, False]]
-        grid.toggle(1, 1)
-        assert grid.grid == [[True, False], [False, False], [False, False]]
+    def test_validate(self) -> None:
+        with pytest.raises(ValueError):
+            # inconsistent column count
+            Grid(
+                [Cell((0, 0), False), Cell((0, 1), True)],
+                [Cell((1, 0), False)],
+            )
+        with pytest.raises(ValueError):
+            # invalid column arrangement
+            Grid(
+                [Cell((0, 0), False), Cell((0, 1), True)],
+                [Cell((1, 1), False), Cell((1, 0), True)],
+            )
+        with pytest.raises(ValueError):
+            # invalid row arrangement
+            Grid(
+                [Cell((1, 0), True), Cell((1, 1), False)],
+                [Cell((0, 0), False), Cell((0, 1), True)],
+            )
 
-        with pytest.raises(IndexError):
-            grid.toggle(1, 3)
+    def test_properties(self, grid: Grid) -> None:
+        assert grid.height == 3
+        assert grid.width == 2
+        assert grid.grid == [
+            Cell((0, 0), False),
+            Cell((0, 1), False),
+            Cell((1, 0), False),
+            Cell((1, 1), False),
+            Cell((2, 0), False),
+            Cell((2, 1), False),
+        ]
+
+    def test_empty_grid(self) -> None:
+        empty_grid = Grid()
+        assert empty_grid == Grid() == Grid([])
+        assert empty_grid.height == empty_grid.width == 0
+
+    def test_grid(self, grid: Grid) -> None:
+        expected = Grid(
+            [Cell((0, 0), False), Cell((0, 1), False)],
+            [Cell((1, 0), False), Cell((1, 1), False)],
+            [Cell((2, 0), False), Cell((2, 1), False)],
+        )
+        assert grid == expected
